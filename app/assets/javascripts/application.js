00874887fc6dd1d9e67f9287851eea5cdd6f06d7
NicoleCarpenter <nicolecarpenterusa@gmail.com>
2015-10-31 13:11:29 -0500

    add footer well to page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,5 +24,6 @@ $(document).ready(function() {
   $('.js-closeToggle').on('click', function() {
     // When clicking on .js-close, find the parent .js-page and add .is-closed to its classlist.
     $('.js-page').toggleClass('is-closed');
-  })
+  });
+
 });
