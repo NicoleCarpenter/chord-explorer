8e76e14a27d28a1dd40ddc012807c3a42ff47b36
Andy Dierker <dierker@gmail.com>
2015-10-30 20:15:10 -0500

    search works, but it's slow

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,22 @@ class WelcomeController < ApplicationController
     if session[:user_id]
       @user = User.find(session[:user_id])
     end
+
+    #change @songs to an array of returned songs once we know how the search is working
+    @songs = Song.all
+
+    if params[:search]
+      chord_objects = params[:search].split(",").map{|chord| Chord.find_by(name: chord.strip)}
+
+      @matching_songs = Song.all.select{|song| chord_objects.to_set.superset?(song.included_chords.map(&:chord).to_set)}
+
+
+      # @chords =
+      # Chord.find_by(name: params[:search])
+      # .order("created_at DESC")
+      # Chord.search(params[:search]).order("created_at DESC")
+    end
+
   end
 
 end
--- app/models/included_chord.rb
+++ app/models/included_chord.rb
@@ -1,4 +1,5 @@
 class IncludedChord < ActiveRecord::Base
   belongs_to  :chord
   belongs_to  :tab
+
 end
