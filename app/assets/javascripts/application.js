b96bd4b3d60f0d6ecbf41da08d6039aebd69b15a
Nicole Carpenter <email": null>
2015-11-01 12:34:49 -0600

    add well for saved chords

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,6 +26,6 @@
 $(document).ready(function() {
   $('.js-closeToggle').on('click', function() {
     // When clicking on .js-close, find the parent .js-page and add .is-closed to its classlist.
-    $('.js-page').toggleClass('is-closed');
+    $('.js-page').slideToggle('slow');
   })
 });
