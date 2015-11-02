11b811e5c39c2c84b7da0f3543e93957c7a25e46
Andy Dierker <dierker@gmail.com>
2015-11-02 16:06:05 -0600

    finished scraping tabs

--- lib/assets/runner.rb
+++ lib/assets/runner.rb
@@ -1,12 +1,14 @@
 require_relative 'artist_scrape'
 require_relative 'fetcher'
+require 'yaml'
 
 # Here we make sure the user has the proper folder structure to contain the scraped JSON objects.
 p Dir.pwd
 
 # Sample usage
 # On the next line, replace text with your name.
-artist_lists = Fetcher.get_em('scraper_links/andy.txt')
+# artist_lists = Fetcher.get_em('scraper_links/andy.txt')
+artist_lists = YAML.load_file('artist_links')
 
 artist_lists.each do |list|
   ArtistSongCrawler.new(list)
