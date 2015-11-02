cf6cab874ef0afb6a75c7d503b9421565d3098e5
Erik Germani <erikg885@gmail.com>
2015-11-01 18:58:01 -0600

    Add next chord recommendation algorithm

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def next_chord(bin_repertoire)
+    current_playable_count = Tab.playables(bin_repertoire).count
+    best_chord = nil
+    Chord.count.times do |i|
+      new_string = bin_repertoire.dup
+      new_string[i] = 1
+      best_chord = i unless Tab.playables(new_string) < current_playable_count
+    end
+    Chord.find(best_chord)
+  end
 end
