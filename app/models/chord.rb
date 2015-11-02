769c80efa5b822fb3d633be568194d86d47acb89
Erik Germani <erikg885@gmail.com>
2015-11-02 15:34:08 -0600

    Include query that will calculate frequency data of chords

--- app/models/chord.rb
+++ app/models/chord.rb
@@ -13,4 +13,9 @@ class Chord < ActiveRecord::Base
     "<div class='chord' data-code='#{self.code}' data-name='#{self.name}'></div>"
   end
 
+  def self.freq_dist
+    # returns a hash with keys, values == chord name, frequency within database
+    IncludedChord.joins(:chord).group("chords.name").order("count_chord_id desc").count("chord_id")
+  end
+
 end
