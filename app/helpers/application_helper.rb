cd37d7d3dd51bdc6e7f3944f679ef5784eebfaac
Andy Dierker <dierker@gmail.com>
2015-11-01 19:30:37 -0600

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,14 @@
 module ApplicationHelper
   def next_chord(bin_repertoire)
     current_playable_count = Tab.playables(bin_repertoire).count
-    best_chord = nil
+    best_chord = ni
     Chord.count.times do |i|
       new_string = bin_repertoire.dup
       new_string[i] = 1
-      best_chord = i unless Tab.playables(new_string) < current_playable_count
+      if Tab.playables(new_string) > current_playable_count
+        best_chord = i
+        current_playable_count = Tab.playables(new_string).count
+      end
     end
     Chord.find(best_chord)
   end
