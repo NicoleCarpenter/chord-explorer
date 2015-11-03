f7e3ba78b8facc53c1907109e314f403d5fa5efa
Andy Dierker & Dennis <pair+adierker.dennisgtam@devbootcamp.com>
2015-11-03 11:01:49 -0600

    working on remove functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -81,8 +81,13 @@ $(document).ready(function() {
 
   //"add saved chords" button will add all saved chords to the current well
   $("body").on("click","#add-saved-chords-button",function(event){
-    console.log("hey");
     $.get("/user_saved_chords")
+  })
+
+  //"forget this chord" button will destroy the saved chord for the user
+  $("body").on("click","#remove-saved-chords-button",function(event){
+    console.log("hey");
+    // $.get("/user_saved_chords")
   })
 
   //this prevents page refresh on Enter. needed for the keyup event below
