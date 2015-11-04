897b7b830cd0f24eeb561b1d750342a864c34b27
Erik Germani <erikg885@gmail.com>
2015-11-03 22:39:22 -0600

    Reenable some commented out lines

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,39 +28,39 @@ proper_chords = Chord.all.pluck(:name)
 
 # A script for getting JSON objects into our database.
 
-# artist_files = Dir.glob('db/artists/*/*.txt')
+artist_files = Dir.glob('db/artists/*/*.txt')
 
-# artist_files.each do |artist_file|
-#   File.foreach(artist_file) do |song_json|
-#     song_data = JSON.parse(song_json)
-#     artist = song_data["artist"].strip
-#     title  = song_data["title"].strip
-#     p "#{artist} - #{title}"
+artist_files.each do |artist_file|
+  File.foreach(artist_file) do |song_json|
+    song_data = JSON.parse(song_json)
+    artist = song_data["artist"].strip
+    title  = song_data["title"].strip
+    p "#{artist} - #{title}"
 
-#     tab_create_hash = {
-#       :domain       => song_data["url"].scan(/(?<=\.).*(?=\.com)/).first,
-#       :view_count   => song_data["ult_guitar_viewcount"].scan(/\d+/).first,
-#       :review_count => song_data["ult_guitar_reviewcount"].scan(/\d+/).first,
-#       :sequence     => Array.new(song_data["chords"]),
-#       :ranking      => song_data["ranking"],
-#       :url          => song_data["url"]
-#     }
+    tab_create_hash = {
+      :domain       => song_data["url"].scan(/(?<=\.).*(?=\.com)/).first,
+      :view_count   => song_data["ult_guitar_viewcount"].scan(/\d+/).first,
+      :review_count => song_data["ult_guitar_reviewcount"].scan(/\d+/).first,
+      :sequence     => Array.new(song_data["chords"]),
+      :ranking      => song_data["ranking"],
+      :url          => song_data["url"]
+    }
 
-#     tab = Tab.new(tab_create_hash)
-#     if tab.save
-#       song = Song.find_or_create_by(artist: artist, title: title)
-#       tab.song = song
-#       tab.save
+    tab = Tab.new(tab_create_hash)
+    if tab.save
+      song = Song.find_or_create_by(artist: artist, title: title)
+      tab.song = song
+      tab.save
 
-#       tab.sequence.uniq.each do |chord_in_song|
-#         chord = Chord.find_by(name: chord_in_song.strip)
-#         IncludedChord.create(chord: chord, tab: tab)
-#       end
-#     else
-#       puts "Contains improper cords"
-#     end
-#   end
-# end
+      tab.sequence.uniq.each do |chord_in_song|
+        chord = Chord.find_by(name: chord_in_song.strip)
+        IncludedChord.create(chord: chord, tab: tab)
+      end
+    else
+      puts "Contains improper cords"
+    end
+  end
+end
 
 puts "Adding frequency data to chords table"
 
@@ -71,25 +71,6 @@ freq_dist.each do |chord_name, frequency|
   chord.frequency = frequency
   chord.save
 end
-
-# cutoff = 3
-
-# puts "Deleting chords with frequency less than #{cutoff}"
-# puts "Current chord count: #{Chord.count}. Current tab count: #{Tab.count}"
-
-# loser_chords = Chord.where("frequency < ?", cutoff)
-# loser_chords.destroy_all
-
-# # Sniping some typo chords...
-
-# Chord.where("name LIKE '%H%'").destroy_all
-
-# # Killing off tabs which have only one chord
-
-# # A hash, keys are tab_ids, values are the amount of chords in them.
-# chords_per_song = IncludedChord.group("chord_id").count("chord_id")
-# chords_per_song.select { |key, value| value == 1}
-# Tab.find(chords_per_song.keys).destroy_all
 
 puts "Escaping sharps and slashes now..."
 
