79651388f2726c813318043cc50b9d2c0a18cd3d
Andy Dierker <dierker@gmail.com>
2015-11-15 16:50:06 -0600

    username and password are required, and uniqueness is true

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   has_many  :user_saved_chords
   has_many  :user_songs
   has_many  :songs, through: :user_songs
-
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true
 end
